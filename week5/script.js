35ba59589e11c5c157709b31b8eefb5e34ef904a
DefenestratedBirb. <130018354+DefenestratedBird@users.noreply.github.com>
2024-08-27 14:53:39 +1000

    break

---
+++ week5/script.js
@@ -0,0 +1,37 @@
+console.log("Welcome to Javascript Practice!");
+//Cries its like C#, cries the pain that I have went through last sem has finally been useful.
+let count = 10;
+console.log(count);
+//const name = prompt("What is your name?");
+const name = "Bob";
+console.log("Hello", name);
+
+//myName is a Camelcase. Camelcases always starts with lowercase.
+const myName = "Lanchu";
+const myCity = "Melbourne";
+console.log(myName, "lives in", myCity);
+
+count = 20;
+console.log(count);
+
+//boolean variable
+let isItRaining = false;
+console.log(isItRaining);
+let isitOARTClass = true;
+console.log(isitOARTClass);
+
+//const studentRecord = {
+//name: "Lanchu",
+//id: 4007083,
+//class: "OART1013"
+//amIScienceStudent: false,
+//amIDesignStudent: true,
+//};
+
+//console.log(studentRecord.name);
+//console.log(studentRecord.name, "is a", studentRecord.class, "student");
+
+//Remember that Arrays "counting" always starts from 0.
+let studentNames = ["Robert", "Alice", "Frank", "Bob", "Tom"];
+console.log(studentNames);
+console.log(studentNames[3]);
