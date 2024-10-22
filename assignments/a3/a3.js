1ba6cbd501b0e9320ed7d5ed464ee990ed5abf65
DefenestratedBirb. <130018354+DefenestratedBird@users.noreply.github.com>
2024-10-22 11:28:52 +1100

    It collides like my braincells at 3am.
    It works?

--- assignments/a3/a3.js
+++ assignments/a3/a3.js
@@ -10,21 +10,37 @@ const walls = document.querySelectorAll(
   ".wall1, .wall2, .wall3, .wall4, .wall5, .wall6, .wall7, .wall8, .wall9, .wall10"
 );
 
-function checkCollision() {
+function checkCollision(newXDistance, newYDistance) {
+  /*Gets the exact position of the ball.*/
   const ballRect = ball.getBoundingClientRect();
+  const tempBallRect = {
+    /*Gets the left position side of the ball and adds the distance that the ball is 
+    about to move based on the player's click of the arrows keys.*/
+    left: ballRect.left + (newXDistance - XDistance),
+    right: ballRect.right + (newXDistance - XDistance),
+    top: ballRect.top + (newYDistance - YDistance),
+    bottom: ballRect.bottom + (newYDistance - YDistance),
+  };
   for (let wall of walls) {
+    /*Gets the exact position of the walls.*/
     const wallRect = wall.getBoundingClientRect();
     if (
-      ballRect.left < wallRect.right &&
-      ballRect.right > wallRect.left &&
-      ballRect.top < wallRect.bottom &&
-      ballRect.bottom > wallRect.top
+      /*If the left position of the wall is smaller than the right position of the wall.*/
+      tempBallRect.left < wallRect.right &&
+      /*If the right position of the wall is larger than the left position of the wall.*/
+      tempBallRect.right > wallRect.left &&
+      /*If the top position of the wall is smaller than the bottom position of the wall.*/
+      tempBallRect.top < wallRect.bottom &&
+      /*If the bottom position of the wall is larger than the top position of the wall.*/
+      tempBallRect.bottom > wallRect.top
     ) {
       console.log("Collision detected with:", wall.id);
       return true;
+      /*Then collision is deteced.*/
     }
   }
   return false;
+  /*Then collision is not deteced.*/
 }
 
 /*Up Button Move*/
@@ -34,15 +50,10 @@ UpButton.addEventListener("click", UpwardsBall);
 
 let YDistance = 0;
 function UpwardsBall() {
-  //If my distance is less then the width of the outerbox do not go outside the outerbox.
-  if (YDistance > -300) {
-    YDistance -= 36;
+  const newYDistance = YDistance - 36;
+  if (YDistance > -300 && !checkCollision(XDistance, newYDistance)) {
+    YDistance = newYDistance;
     UpdateBallPosition();
-    checkCollision();
-    {
-      YDistance += 30;
-    }
-    /*transformBall(); For da reset button.*/
   }
 }
 
@@ -52,11 +63,10 @@ console.log(DownButton);
 DownButton.addEventListener("click", DownwardsBall);
 
 function DownwardsBall() {
-  //If my distance is less then the width of the outerbox do not go outside the outerbox.
-  if (YDistance < 20) {
-    YDistance += 35;
+  const newYDistance = YDistance + 35;
+  if (YDistance < 20 && !checkCollision(XDistance, newYDistance)) {
+    YDistance = newYDistance;
     UpdateBallPosition();
-    /*transformBall(); For da reset button.*/
   }
 }
 
@@ -69,11 +79,10 @@ LeftButton.addEventListener("click", LeftBall);
 let XDistance = 0;
 
 function LeftBall() {
-  //If my distance is less then the width of the outerbox do not go outside the outerbox.
-  if (XDistance > 0) {
-    XDistance -= 30;
+  const newXDistance = XDistance - 30;
+  if (XDistance > 0 && !checkCollision(newXDistance, YDistance)) {
+    XDistance = newXDistance;
     UpdateBallPosition();
-    /*transformBall(); For da reset button.*/
   }
 }
 
@@ -82,12 +91,9 @@ console.log(RightButton);
 RightButton.addEventListener("click", RightBall);
 
 function RightBall() {
-  //If my distance is less then the width of the outerbox do not go outside the outerbox.
-  if (XDistance < 450) {
-    XDistance += 30;
+  const newXDistance = XDistance + 30;
+  if (XDistance < 450 && !checkCollision(newXDistance, YDistance)) {
+    XDistance = newXDistance;
     UpdateBallPosition();
-
-    console.log(XDistance);
-    /*transformBall(); For da reset button.*/
   }
 }
