4c78ae9498240e0af463cb20ab45c43d96b101cc
DefenestratedBirb. <130018354+DefenestratedBird@users.noreply.github.com>
2024-10-22 14:12:30 +1100

    Quick Update

--- assignments/a3/a3.js
+++ assignments/a3/a3.js
@@ -3,11 +3,11 @@ console.log(ball);
 
 function UpdateBallPosition() {
   ball.style.transform = `translate(${XDistance}px, ${YDistance}px)`;
-  console.log(`translateX(${XDistance}px), translateY(${YDistance}px)`);
+  /*console.log(`translateX(${XDistance}px), translateY(${YDistance}px)`);*/
 }
 
 const walls = document.querySelectorAll(
-  ".wall1, .wall2, .wall3, .wall4, .wall5, .wall6, .wall7, .wall8, .wall9, .wall10"
+  "#wall1, #wall2, #wall3, #wall4, #wall5, #wall6, #wall7, #wall8, #wall9, #wall10"
 );
 
 function checkCollision(newXDistance, newYDistance) {
@@ -15,7 +15,7 @@ function checkCollision(newXDistance, newYDistance) {
   const ballRect = ball.getBoundingClientRect();
   const tempBallRect = {
     /*Gets the left position side of the ball and adds the distance that the ball is 
-    about to move based on the player's click of the arrows keys.*/
+    about to move based on the player's click of the arrows buttons.*/
     left: ballRect.left + (newXDistance - XDistance),
     right: ballRect.right + (newXDistance - XDistance),
     top: ballRect.top + (newYDistance - YDistance),
@@ -49,8 +49,11 @@ console.log(UpButton);
 UpButton.addEventListener("click", UpwardsBall);
 
 let YDistance = 0;
+let newYDistance = 0;
+let newXDistance = 0;
+
 function UpwardsBall() {
-  const newYDistance = YDistance - 36;
+  let newYDistance = YDistance - 36;
   if (YDistance > -300 && !checkCollision(XDistance, newYDistance)) {
     YDistance = newYDistance;
     UpdateBallPosition();
@@ -63,7 +66,7 @@ console.log(DownButton);
 DownButton.addEventListener("click", DownwardsBall);
 
 function DownwardsBall() {
-  const newYDistance = YDistance + 35;
+  let newYDistance = YDistance + 35;
   if (YDistance < 20 && !checkCollision(XDistance, newYDistance)) {
     YDistance = newYDistance;
     UpdateBallPosition();
@@ -79,7 +82,7 @@ LeftButton.addEventListener("click", LeftBall);
 let XDistance = 0;
 
 function LeftBall() {
-  const newXDistance = XDistance - 30;
+  let newXDistance = XDistance - 30;
   if (XDistance > 0 && !checkCollision(newXDistance, YDistance)) {
     XDistance = newXDistance;
     UpdateBallPosition();
@@ -91,9 +94,21 @@ console.log(RightButton);
 RightButton.addEventListener("click", RightBall);
 
 function RightBall() {
-  const newXDistance = XDistance + 30;
+  let newXDistance = XDistance + 30;
   if (XDistance < 450 && !checkCollision(newXDistance, YDistance)) {
     XDistance = newXDistance;
     UpdateBallPosition();
   }
+}
+
+/*Reset X/Y Distance Transform Position of Ball on collision.*/
+const obstacle = document.querySelectorAll(
+  "#obs1, #obs2, #obs3, #obs4, #obs5, #obs6, #obs7, #obs8, #obs9, #obs10",
+  ResetPosition
+);
+
+function ResetPosition() {
+  newYDistance = 0;
+  newXDistance = 0;
+  UpdateBallPosition();
 }
